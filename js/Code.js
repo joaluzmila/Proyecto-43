6950804d388be916021e1ae47ad2af04f2db334e
joaluzmila <86448563+joaluzmila@users.noreply.github.com>
2021-12-29 15:07:55 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: Siempre cambia, no es constante !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("C U N I F O N", 700,150)
+fill("lightblue")
+text("Pista: Realiza tareas específicas !!", 700,170)
+
+fill("white")
+    textSize(15)
+    text("E D A T E D O S B A S", 100,310)
+    fill("lightblue")
+    text("Pista: Almacena toda la información !!", 100,330)   
+
+}
